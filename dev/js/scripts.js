48bb094977e1855d4cb10b3c2d769d2b1ac23097
queeniechen79 <63672423+queeniechen79@users.noreply.github.com>
2021-06-10 03:39:00 -0400

    added more stuff

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -62,7 +62,7 @@ ready(() => {
       let tl = gsap.timeline();
 
       tl.from ("#lexus", {duration:2, alpha:0})
-        .to ("#lexus", {duration:1.5, y:"+=800"})
+        .to ("#lexus", {duration:1, scale:0, transformOrigin:"center"})
     
       ;//tl END
   
@@ -73,10 +73,11 @@ ready(() => {
   //***********  speedometerTL  ****************
   function speedometerTL(){
     let tl = gsap.timeline();
-    gsap.set ("#speedometer-circle", {transformOrigin:"center center"})
 
-    tl.from ("#speedometer-circle", {duration:1, ease:"none", drawSVG: 0})
-    .to ("#speedometer-circle", {duration:1, ease:"none" })
+    gsap.set ("#speedometer-circle", {transformOrigin:"10% 90%"})
+
+    tl.from ("#speedometer-circle", {duration:0.5, ease:"none", drawSVG: 0})
+    .to ("#speedometer-circle", {duration:0.5, ease:"none" })
     .from ("#tick-1" ,{duration:0.1, alpha:0})
     .from ("#tick-2" ,{duration:0.1, alpha:0})
     .from ("#tick-3" ,{duration:0.1, alpha:0})
@@ -89,10 +90,6 @@ ready(() => {
     .from ("#tick-10" ,{duration:0.1, alpha:0})
     .from ("#tick-11" ,{duration:0.1, alpha:0})
     .from ("#tick-12" ,{duration:0.1, alpha:0})
-    .from ("#fuel-gauge" ,{duration:0.5, alpha:0})
-    .from ("#E" ,{duration:0.5, alpha:0}, "ef")
-    .from ("#F" ,{duration:0.5, alpha:0}, "ef")
-    .from ("#fuel-bar", {duration:0.5, scale:0 })
 
 
     ;//tl END
@@ -101,7 +98,7 @@ ready(() => {
 
   }
 
-  var counterSpeed = 100;
+  var counterSpeed = 50;
 
   // Set this number you want your counter to count up to. Default is 20
   var topSpeed = 40;
@@ -119,56 +116,89 @@ ready(() => {
     } else {
       clearInterval(myVar);
     }
+
+  
     return speedNumber;
   }
 
+    //*********** fuelTL ****************
+  function fuelTL(){
+      let tl = gsap.timeline();
+      tl.from ("#fuel-gauge" ,{duration:0.5, alpha:0})
+      .from ("#E" ,{duration:0.5, alpha:0}, "ef")
+      .from ("#F" ,{duration:0.5, alpha:0}, "ef")
+      .from ("#fuel-bar", {duration:0.5, scale:0 })
+      .to ("#fuel-shape", {duration:0.5, x:"+=60"})
+      ;//tl END
+  
+      return tl;
+  
+    }
+
   //*********** mapTL ****************
-  // function mapTL(){
-  //   let tl = gsap.timeline();
-  //     gsap.set ("#map-circle", {drawSVG: "0%"})
-  //     gsap.set ("#map-lines", {drawSVG: "0%"})
-  //     tl.from ("#map-circle", {duration:1, ease:"power2.out", drawSVG: "0%"})
-  //     tl.from ("#map-lines", {duration:1, ease:"power2.out", drawSVG: "0%"})
-  //       .from ("#navigation-icon", {duration:1, alpha:0})
-  //       .from ("#p", {duration:0.5, alpha:0, x:"-=50"})
-  //       .from ("#r", {duration:0.5, alpha:0, x:"-=50"})
-  //       .from ("#n", {duration:0.5, alpha:0, x:"-=50"})
-  //       .from ("#d", {duration:0.5, alpha:0, x:"-=50"})
-  //       .to ("#p", {scale:1.5, duration: 0.5, color: "065EB1"})
-  //   ;//tl END
+  function mapTL(){
+    let tl = gsap.timeline();
 
-  //   return tl;
+    gsap.set ("#map-circle", {transformOrigin:"center"})
+    gsap.set ("#Line, #Line-2,#Line-3,#Line-4,#Line-5,#Line-6,#Line-7,#Line-8,#Line-9,#Line-10,#Line-11,#Line-12,#Line-13", {transformOrigin: "bottom"})
 
-  // }
+    tl.from ("#map-circle", {duration:0.5, ease:"none", drawSVG: 0})
+      .to ("#map-circle", {duration:0.5, ease:"none" })
+        
+    // .from ("#map-lines", {duration:1, ease:"none", drawSVG: 0})
+    // .to ("#map-lines", {duration:1, ease:"none"})
+
+    .from ("#Line, #Line-2,#Line-3,#Line-4,#Line-5,#Line-6,#Line-7,#Line-8,#Line-9,#Line-10,#Line-11,#Line-12,#Line-13", {duration:1, ease:"none", drawSVG: 0})
+    .to ("#Line, #Line-2,#Line-3,#Line-4,#Line-5,#Line-6,#Line-7,#Line-8,#Line-9,#Line-10,#Line-11,#Line-12,#Line-13", {duration:1, ease:"none"}, "map")
+
+    .from ("#navigation-icon", {duration:1, alpha:0, delay:0.5}, "map")
+    .from ("#P", {duration:0.5, alpha:0, x:"-=50"}, "prnd")
+    .from ("#R", {duration:0.5, delay:0.1, alpha:0, x:"-=50"}, "prnd")
+    .from ("#N", {duration:0.5, delay:0.2, alpha:0, x:"-=50"}, "prnd")
+    .from ("#D", {duration:0.5, delay:0.3, alpha:0, x:"-=50"}, "prnd")
+    .to ("#P", {scale:1.5, duration: 0.5, fill: "#065EB1"})
+    ;//tl END
+
+    return tl;
+
+  }
 
 //*********** otherTL ****************
 
-  // function otherTL(){
-  //   let tl = gsap.timeline();
+  function otherTL(){
+    let tl = gsap.timeline();
 
-  //   tl.from ("#other", {duration:1, alpha:0}, "bottom")
+    tl.from ("#MAY30", {duration:0.5, alpha:0, y:"+=20"})
+      .from ("#Cloudy", {duration:0.5, alpha:0, x:"-=20"}, "other")
+      .from ("#weather", {duration:0.5, alpha:0, x:"-=20"}, "other")
+      .from ("#time", {duration:0.5, alpha:0, x:"-=20"}, "other")
    
-  //   ;//tl END
+    ;//tl END
 
-  //   return tl;
+    return tl;
 
-  // }
+  }
 
   //*********** musicTL ****************
-  // function musicTL(){
-  //   let tl = gsap.timeline();
+  function musicTL(){
+    let tl = gsap.timeline();
 
-  //   tl.from ("#music", {duration:1, alpha:0}, "bottom")
-  //     gsap.set ("#play-button", {display:"none"})
-  //     .to ("#music-time-bar", {duration:3, scaleX:4}, "music")
-  //     .to ("#Lemon-by-Kenshi-Yone", {duration:3, x:"+=15"}, "music")
+    tl.from ("#album-cover", {duration:1, alpha:0})
+      gsap.set ("#play-button", {display:"none"})
+      .from ("#lemon", {duration:0.5, alpha:0})
+      .to ("#music-time-bar", {duration:3, scaleX:4})
+      .from ("#upnext", {duration:0.5, alpha:0, y:"-=20"})
+      .from ("#song-2", {duration:0.5, alpha:0, x:"-=20"})
+      .from ("#NavillerabyG-friend", {duration:0.5, alpha:0, x:"-=20"})
+
+     
    
      
-  //   ;//tl END
+    ;//tl END
 
-  //   return tl;
+    return tl;
 
-  // }
+  }
 
   //*********** phoneTL ****************
   // function phoneTL(){
@@ -195,9 +225,10 @@ gsap.set('#svg-container',{visibility:"visible"});
 mainTL.add(logoTL())
       .add(speedometerTL())
       .add(speedCounter())
-      // .add(mapTL())
-      // .add(otherTL())
-      // .add(musicTL())
+      .add(fuelTL())
+      .add(mapTL())
+      .add(otherTL())
+      .add(musicTL())
       // .add(phoneTL())
 
 
